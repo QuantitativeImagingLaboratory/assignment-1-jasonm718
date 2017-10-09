a119c47e5bf24beba245a3be1c633fc9eb867f17
jasonm718 <jasonm718@gmail.com>
2017-10-08 21:29:31 -0500

    Could not get region counting to work

--- region_analysis/cell_counting.py
+++ region_analysis/cell_counting.py
@@ -1,3 +1,7 @@
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+
 class cell_counting:
 
     def blob_coloring(self, image):
@@ -6,8 +10,44 @@ class cell_counting:
         image: binary image
         return: a list of regions"""
 
-        regions = dict()
+        w, h = image.shape
 
+        regions = dict()
+        k = 1
+
+        R = np.zeros((w, h), dtype=np.uint32)
+
+        for i in range(0,w):
+
+            for j in range(0,h):
+                #print(regions)
+               # if (R[i, j - 1] == 0 or R[i - 1, j] == 0):
+
+                #print(i, j, R[i, j - 1], R[i - 1, j], image[i, j], image[i - 1, j], image[i, j - 1])
+
+                if(image[i][j] == 255 and image[i][j-1]== 0 and image[i-1][j] == 0):
+                    R[i,j] = k
+                    regions[k] = [(i,j)]
+                    k += 1
+
+                elif(image[i][j] == 255 and image[i][j-1] == 0 and image[i-1][j] == 255):
+                    R[i, j] = R[i-1, j]
+                    regions[R[(i, j)]].append((i, j))
+
+                elif (image[i][j] == 255 and image[i][j-1] == 255 and image[i-1][j] == 0):
+
+                    R[i, j] = R[i, j - 1]
+                    regions[R[i, j]].append((i, j))
+
+                elif (image[i][j] == 255 and image[i][j-1] == 255 and image[i-1][j] == 255):
+                    R[i, j] = R[i-1, j]
+
+                    if R[i, j - 1] != R[i - 1, j]:
+                        R[i,j-1] = R[i-1,j]
+
+                    regions[R[i, j]].append((i, j))
+
+        print(regions.keys())
         return regions
 
     def compute_statistics(self, region):
@@ -30,6 +70,11 @@ class cell_counting:
         image: a list of pixels in a region
         stats: stats regarding location and area
         returns: image marked with center and area"""
+        plt.clf()
+        plt.imshow(image, interpolation= 'none', cmap=plt.cm.gray)
+
+        plt.show()
+        plt.savefig('output/cellct/result.png')
 
         return image
 
--- report.md
+++ report.md
@@ -1,1 +1,18 @@
+1) Resampling
 
+First i figured out how to do nearest neighbor. Then proceeded to linear interpolation. Bilinear interpolation was just
+the linear interpolation of two linear interpolations. Basically, followed the algorithm in the slides.
+
+
+2) Region counting
+
+Was able to create a histogram of the pixels. Then found the threshold through finding
+the peaks of the histogram and getting the average. Found "T". Algorithm from the slides
+
+Could not get the cell counting to work.
+Problems
+-   for some reason i get negative values in row 0 when there shouldn't be a pixel before 0
+
+
+
+*This assignment was extremely difficult and had a huge initial learning curve*
